77f3d8b852cf730e9536d330b6764ec3a95f69a7
ninehundred1 <meyernsen@gmail.com>
2015-07-31 17:25:54 -0400

    added contrast

--- relish/relish.py
+++ relish/relish.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 '''Resize a group of images to a predefined or custom resolution'''
 
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 import sys
 from os.path import expanduser
@@ -11,10 +11,8 @@ def dir_list(indir):
     '''Return a list of files as strings'''
     return os.listdir(indir)
 
-def enlarge_size(indir, outdir, new_width):
-    return
 
-def shrink_size(indir, outdir, new_width):
+def resize(indir, outdir, new_width):
     '''resize images based on parameters using the PILLOW library'''
 
     images = dir_list(indir)
@@ -35,24 +33,89 @@ def shrink_size(indir, outdir, new_width):
         print('Processed {} - {} of {}'.format(image, i, numpics))
         i += 1
 
+    print "\nFinished. \nAnything else?\n"
 
-def pic_parameters(infolder, outfolder):
-    '''Select Picture Resolution Parameters'''
 
-    # for 'sizes' only need the width as we will calculate the
-    # height based on a percentage to keep the proper aspect ratio when
-    # resizing
+def apply_filter_contrast(indir, outdir):
+
+    print "Amount of contrast change (0-10)" \
+              "<1: lower contrast, >1: increase contrast."
+
     while True:
 
-        print('(S)hrink or (E)nlarge?')
-        size_operation = raw_input('>: ')
-        possible_inputs = {'S','s','E','e'}
+        contrast_modifier = raw_input('>: ')
 
-        if size_operation not in possible_inputs:
+        if float(contrast_modifier) < 0 or float(contrast_modifier) > 10:
+            print "enter number in range"
+        else:
+            break
+
+
+    picture_list = dir_list(indir)
+    counter_processed = 1
+    img_quality = 95
+    numpics = len(picture_list)
+
+    for current_file_name in picture_list:
+        image_object = Image.open(indir + current_file_name)
+        contrast_object = ImageEnhance.Contrast(image_object)
+        contrast_object.enhance(float(contrast_modifier)).save(outdir + current_file_name, quality=img_quality)
+        print('Processed {} - {} of {}'.format(current_file_name, counter_processed, numpics))
+        counter_processed += 1
+
+    print "\nFinished. \nAnything else?\n"
+
+
+
+
+def filter_switch_function(case, indir, outdir):
+    return {
+        'E': apply_filter_contrast(indir, outdir),
+    }
+
+
+
+def filter_images(indir, outdir):
+
+    while True:
+
+        print('''Choose filter:
+          (E)nhance contrast,
+          (T)ile to mosaic,
+          (B)lack and white,
+          (D)ramaticize,
+          (A)dd black frame,
+          (R)ETURN TO MENU''')
+
+        filter_choice = raw_input('>: ')
+
+        if filter_choice == 'r' or filter_choice == 'R':
+            break
+
+        list_of_choices = {'E','e','T','t','B','b','D','d','A','a'}
+
+        if filter_choice not in list_of_choices:
             print 'Invalid entry, try again...'
             continue
+        else:
+            filter_switch_function(filter_choice, indir + '/', outdir + '/',)
+            break
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+def resize_images(infolder, outfolder):
+    while True:
         sizes = {
         'P': '2376',
         'L': '1024',
@@ -75,34 +138,59 @@ def pic_parameters(infolder, outfolder):
           (E)mail,
           (S)mall,
           (T)humbnail
-          (R)ETURN TO MENU
-          (Q)UIT''')
+          (R)ETURN TO MENU''')
 
         size = raw_input('>: ')
 
         if size == 'r' or size == 'R':
-            continue
-
-        if size == 'Q' or size == 'q':
-            print 'quit.'
             break
 
         if size in sizes.keys():
 
-            shrink_size(
+            resize(
                 infolder + '/', outfolder + '/', sizes[size]
                 )
-
-            enlarge_size(
-                infolder + '/', outfolder + '/', sizes[size]
-                )
-
-
         else:
             print 'Invalid entry, try again...'
 
         pic_parameters(infolder, outfolder)
         break
+
+
+
+
+def pic_parameters(infolder, outfolder):
+    '''Select Picture Resolution Parameters'''
+
+    # for 'sizes' only need the width as we will calculate the
+    # height based on a percentage to keep the proper aspect ratio when
+    # resizing
+    while True:
+
+        print('(R)esize, (A)dd Filter or (q)uit?')
+        pic_operation = raw_input('>: ')
+        possible_inputs = {'R','r','A','a'}
+
+        if pic_operation == 'Q' or pic_operation == 'q':
+            print "quit."
+            break
+
+        if pic_operation not in possible_inputs:
+            print 'Invalid entry, try again...'
+            continue
+
+        if pic_operation == 'r' or pic_operation == 'R':
+            resize_images(infolder, outfolder)
+            break
+        else:
+            filter_images(infolder, outfolder)
+
+
+
+
+
+
+
 
 
 def main():
@@ -135,11 +223,12 @@ def main():
                 trailing "/"')
 
         infolder = raw_input('Source folder:> ')
-        outfolder = raw_input('Destination folder(S for same):> ')
+        outfolder = raw_input('Destination folder (s for same):> ')
     if 'S' in outfolder or 's' in outfolder:
         outfolder = infolder
 
     pic_parameters(infolder, outfolder)
+
 
 
 if __name__ == '__main__':
